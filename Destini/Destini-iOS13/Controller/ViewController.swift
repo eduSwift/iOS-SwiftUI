f073562e169d82a3beed9c0fb5c3e18f10b88a48
eduSwift <eduSwift@yahoo.com>
2024-06-17 12:50:42 +0200

    gitignore

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -16,9 +16,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let story0  = "You see a fork in the road."
+        let choice1 = "Take a left."
+        let choice2 = "Take a right."
+        
+        storyLabel.text = story0
+        choice1Button.setTitle("Take a left.", for: .normal)
+        choice2Button.setTitle("Take a right.", for: .normal)
     }
+    
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 }
 
