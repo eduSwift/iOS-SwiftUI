8f349586f9564df20c94d4bc77923b304fbdc0e2
eduSwift <eduSwift@yahoo.com>
2024-06-27 18:18:51 +0200

    JSON

--- Clima/Clima/WeatherManager.swift
+++ Clima/Clima/WeatherManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct WeatherManager {
     let weatherURL =
-        "https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=786326a208afc328ca387055150fd6dd&units=metric"
+    "https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=786326a208afc328ca387055150fd6dd&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -26,7 +26,17 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             //3. Give the session a task
             
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            
             //4. Start the task
             
             task.resume()
@@ -34,15 +44,14 @@ struct WeatherManager {
         
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
         
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
     }
 }
