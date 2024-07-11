38fd52f68f75732a7155949b003a46b6ff1cc060
eduSwift <eduSwift@yahoo.com>
2024-07-11 14:08:42 +0200

    init

---
+++ H4X0R/H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Eduardo Rodrigues on 11.07.24.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
