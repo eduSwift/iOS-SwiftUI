16b36eeebb71d25e3220bc81c01dba7d55d46caa
eduSwift <eduSwift@yahoo.com>
2024-07-11 15:53:26 +0200

    init

--- H4X0R-News/H4X0R-News/ContentView.swift
+++ H4X0R-News/H4X0R-News/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  H4X0R
+//  H4X0R-News
 //
 //  Created by Eduardo Rodrigues on 11.07.24.
 //
@@ -9,15 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        
             Text("Hello, world!")
         }
-        .padding()
+     
     }
-}
+
 
 #Preview {
     ContentView()
--- H4X0R-News/H4X0R-News/H4X0R_NewsApp.swift
+++ H4X0R-News/H4X0R-News/H4X0R_NewsApp.swift
@@ -1,6 +1,6 @@
 //
-//  H4X0RApp.swift
-//  H4X0R
+//  H4X0R_NewsApp.swift
+//  H4X0R-News
 //
 //  Created by Eduardo Rodrigues on 11.07.24.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct H4X0RApp: App {
+struct H4X0R_NewsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
