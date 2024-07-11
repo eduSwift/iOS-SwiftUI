0fb19a864a9fa84127169b54dc0aba9e0be4b243
eduSwift <eduSwift@yahoo.com>
2024-07-11 14:02:05 +0200

    complete app

--- Dicee-2.0/Dicee-2.0/ContentView.swift
+++ Dicee-2.0/Dicee-2.0/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
+    
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -15,15 +20,45 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
                 Image("diceeLogo")
-                Image("dice1")
-                    .resizable()
-                
+                Spacer()
+
+                HStack {
+                    DiceView(n:leftDiceNumber)
+                    DiceView(n:rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                    .background(Color.red)
+                Spacer()
+                }
             }
         }
-        
     }
-}
+                         
 
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
 }
