e0e93d9e8f202cfce1af5bf5bf78f671550bb7fe
eduSwift <eduSwift@yahoo.com>
2024-09-04 20:14:55 +0200

    db update Data with RealmSwift

--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -56,6 +56,17 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+            }
+           
+        } catch {
+            print("Error saving done status, \(error)")
+        }
+        
+            tableView.reloadData()
         //itemArray.remove(at: indexPath.row)
         //context.delete(itemArray[indexPath.row])
         
