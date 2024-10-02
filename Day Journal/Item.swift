2072eb0e48e4e1443551df3366b6b2d039b36d91
eduSwift <eduSwift@yahoo.com>
2024-10-02 16:31:06 +0200

    db update

---
+++ Day Journal/Item.swift
@@ -0,0 +1,31 @@
+//
+//  Item.swift
+//  Day Journal
+//
+//  Created by Eduardo Rodrigues on 02.10.24.
+//
+
+import Foundation
+
+import Foundation
+import SwiftData
+
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
+
+@Model
+final class Todo {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
