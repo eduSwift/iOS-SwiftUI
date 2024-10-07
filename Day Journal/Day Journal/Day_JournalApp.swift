c5ec5da03d47ed5a17635f5afae8db65574bf118
eduSwift <eduSwift@yahoo.com>
2024-10-07 21:37:04 +0200

    db update

--- Day Journal/Day Journal/Day_JournalApp.swift
+++ Day Journal/Day Journal/Day_JournalApp.swift
@@ -5,3 +5,13 @@
 //  Created by Eduardo Rodrigues on 22.09.24.
 //
 
+import SwiftUI
+
+@main
+struct Day_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JournalEntriesListView()
+        }
+    }
+}
